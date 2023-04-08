cda02f50da76e19d00994d98b07f1812bb80039b
sametuca <sametuca@hotmail.com>
2023-04-08 10:21:01 +0300

    added consumerApp comment

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -2,13 +2,16 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
+//Bağlantı oluşturma
 ConnectionFactory factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://eqafabgk:VWWkNnuR6iuDu27BQyVoFB3NwWzRC0rV@chimpanzee.rmq.cloudamqp.com/eqafabgk");
 
+//Bağlantıyı aktifleştirme ve kanal açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 channel.QueueDeclare("example-queue", true, false, false, null);
 
+//Mesaj alma
 EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
 channel.BasicConsume(queue:"example-queue", autoAck:false, consumer:consumer);
 consumer.Received += (model, ea) =>
